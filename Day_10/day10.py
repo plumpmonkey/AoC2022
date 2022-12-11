356b38c7f23e8fb52da8bcb2097f85edb1faca11
Jon White <white.jon@gmail.com>
2022-12-11 20:43:04 +0000

    Day 10 Complete!

--- Day_10/day10.py
+++ Day_10/day10.py
@@ -4,7 +4,7 @@ import os
 
 
 dirname = os.path.dirname(__file__)
-input_file = os.path.join(dirname, 'sample.txt')
+input_file = os.path.join(dirname, 'input.txt')
 
 def part1(data):
     print("Part 1")
@@ -78,52 +78,60 @@ def part2(data):
 
     cycle_count = 0
 
-    total = 0
-
     X = 1
 
     # Create a list with NUM_CYCLES elements
     X_list = [0] * (NUM_CYCLES + 1)
         
-
+    # For Part 2, we will create a list with the value of X at each cycle,
+    # then we can parse the list to print to the screen.
     for instruction in data:
         # print(f"Instruction: {instruction}")
-
 
         if instruction == "noop":
             X_list[cycle_count] = X
             cycle_count += 1
 
-            print(f"X_list[{cycle_count}]: {X_list[cycle_count]}")
-
+            # print(f"X_list[{cycle_count}]: {X_list[cycle_count]}")
 
         elif instruction[:4] == "addx":
             X_list[cycle_count] = X
             cycle_count += 1
 
-            print(f"X_list[{cycle_count}]: {X_list[cycle_count]}")
-            
+            # print(f"X_list[{cycle_count}]: {X_list[cycle_count]}")
 
             # get the value to add to X from the instruction. The value is split from the instruction by a space
             value = int(instruction.split(" ")[1])
 
             X_list[cycle_count] = X
 
-            print(f"X_list[{cycle_count}]: {X_list[cycle_count]}")
+            # print(f"X_list[{cycle_count}]: {X_list[cycle_count]}")
             
             cycle_count += 1
 
             X += value
 
+    # print(X_list)
+
     # Print to the screen
     for row in range(CRT_ROWS):
         for pixel in range(CRT_COLS):
-            char_to_print = '.'
 
+            # We must print a # if the current X value +/-1 is equal to the current pixel value
             # 
-            if abs(X_list[row*40 + pixel] - pixel) < 1:
+            # ((row*40) + pixel) gives us an index into the X_list
+            # 
+            # if we then take the signal value, and subtract the current pixel value, 
+            # then if it is -1, 0 or 1, we match our criteria of +/- 1.
+            # Therefore, if we abs() the result, we can check if it is <= 1
+            #
+            if abs(X_list[row*40 + pixel] - int(pixel)) <= 1:
                 char_to_print = '#'
+            else:
+                # Print a space (not a dot -for readability)
+                char_to_print = ' '
 
+            # Print the character, do not print a new line
             print(char_to_print, end='')
 
         print()
