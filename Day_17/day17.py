e617b83d331411e79d7370948a2726faa27a93fa
Jon White <white.jon@gmail.com>
2022-12-28 15:40:13 +0000

    Day 17 Part 2 complete!

--- Day_17/day17.py
+++ Day_17/day17.py
@@ -5,10 +5,10 @@ from enum import Enum
 import itertools
 import time
 
-DRAW_BOARD = True
+DRAW_BOARD = False
 
 dirname = os.path.dirname(__file__)
-input_file = os.path.join(dirname, 'sample.txt')
+input_file = os.path.join(dirname, 'input.txt')
 
 # A enum for the different types of shapes 
 # contains a set of all the points that the shape occupies
@@ -41,6 +41,7 @@ class Colours(Enum):
     BOLD = "\033[1m"
     NORMAL = "\033[0m"
          
+CACHE_CHECK_RANGE = 20
 
 # A class to add a vector and return a new Point. 
 # Called to blow a shape left, right, or let it fall
@@ -76,33 +77,33 @@ class Shape:
     # Returns boolean: True if the shape moved, False if it cant move
     def move(self, direction, resting_points):
         if direction == '<':
-            print(Colours.YELLOW.value + 'Move left' + Colours.NORMAL.value)
+            # print(Colours.YELLOW.value + 'Move left' + Colours.NORMAL.value)
 
             # find minimum x value in self.points 
             min_x = min(point.x for point in self.points)
 
             if min_x == Board.L_WALL + 1:
-                print(Colours.RED.value + 'Hit left wall' + Colours.NORMAL.value)
+                # print(Colours.RED.value + 'Hit left wall' + Colours.NORMAL.value)
                 return False            
             
         elif direction == '>':
-            print(Colours.YELLOW.value + 'Move right' + Colours.NORMAL.value)
+            # print(Colours.YELLOW.value + 'Move right' + Colours.NORMAL.value)
 
             # find maximum x value in self.points
             max_x = max(point.x for point in self.points)
 
             if max_x == Board.R_WALL - 1:
-                print(Colours.RED.value + 'Hit right wall' + Colours.NORMAL.value)
+                # print(Colours.RED.value + 'Hit right wall' + Colours.NORMAL.value)
                 return False
 
         elif direction == 'V':
-            print(Colours.YELLOW.value + 'Move down' + Colours.NORMAL.value)
+            # print(Colours.YELLOW.value + 'Move down' + Colours.NORMAL.value)
             
             # find minimum y value in self.points
             min_y = min(point.y for point in self.points)
 
             if min_y == Board.FLOOR + 1:
-                print(Colours.RED.value + 'Hit floor' + Colours.NORMAL.value)
+                # print(Colours.RED.value + 'Hit floor' + Colours.NORMAL.value)
                 self.at_rest = True
                 return False
         else:
@@ -112,13 +113,13 @@ class Shape:
 
         # Check if there is a collision in the two sets of points
         if candidate_points & resting_points:
-            print(Colours.RED.value + 'Collision' + Colours.NORMAL.value)
+            # print(Colours.RED.value + 'Collision' + Colours.NORMAL.value)
             return False
         else:
             # Move the shape by the direction
             self.points = {point + Point(*MOVE[direction]) for point in self.points}
 
-            print(f"Updated coords: {self.shape_type} {self.points}")
+            # print(f"Updated coords: {self.shape_type} {self.points}")
 
             return True
     
@@ -126,7 +127,7 @@ class Shape:
     @classmethod
     def create_shape_by_type(self, shape_type, offset : set[Point]):
 
-        print(f"Creating new shape at offset {offset}")
+        # print(f"Creating new shape at offset {offset}")
 
         # Create a new shape instance
         # Loop through the points in the shape type and add the starting position offset
@@ -168,22 +169,28 @@ class Board:
         self.shape_generator = itertools.cycle(enumerate(ShapeType))    # A generator to cycle through the shapes (loops)
         self.jetPattern = itertools.cycle(enumerate(jetPattern))        # A generator to cycle through the jet pattern (loops)
         self.rock_origin = Point(Board.START_X, Board.START_Y)          # The starting position of a new rock taking into account left wall and floor
+        self.active_shape = None                                        # The current shape that is falling
         self.resting_shapes = set()                                     # A set of shapes that are resting on the board
         self.resting_points = set()                                     # A set of points that are occupied by resting shapes
+        self.find_repeating_pattern = False                             # Flag to indicate if a repeating pattern should be found
+        self.repeat_found = False                                       # Flag to indicate if a repeating pattern has been found
+        self.pattern_cache = {}                                         # A cache of patterns that have been seen (key = shape index, jet index, string of last 20 rows)
+        self.debug = False
 
     def new_shape(self):
         shape_index, shape_type = next(self.shape_generator)
 
         self.active_shape = Shape.create_shape_by_type(shape_type, self.rock_origin)
 
-        print(f'New shape: {self.active_shape}')
+        if self.debug:
+            print(f'New shape: {self.active_shape}')
 
         # Loop through the jet pattern until the shape is at rest
         while not self.active_shape.at_rest:
             # Get the next jet pattern
             jet_index, jet_direction = next(self.jetPattern)
 
-            print(f'Jet: {jet_index} - {jet_direction}')
+            # print(f'Jet: {jet_index} - {jet_direction}')
 
             # Move the shape for the jet direction
             self.active_shape.move(jet_direction, self.resting_points)
@@ -206,21 +213,104 @@ class Board:
                 # to the new shape location
                 shape_max_y = max(point.y for point in self.active_shape.points) 
                 
-                print(f"Resting shape max y {shape_max_y}")
-
                 if shape_max_y > self.top:
                     self.top = shape_max_y
                         
-                    print(f"New top: {self.top}")
+                    # print(f"New top: {self.top}")
 
                     # Update the rock origin to the new top of board
                     self.rock_origin = Point(Board.START_X, Board.START_Y + self.top)
+
+                # 
+                # Part 2 functionality
+                # 
+                # Check if this shape index, jet index and the last CACHE_CHECK_RANGE rows of the board match a previously seen
+                # pattern. If so, then we have found a repeating pattern and can stop the game.
+                #
+                # The pattern is stored as a string of the last CACHE_CHECK_RANGE rows of the board, with each row being
+                # a string of the printable characters for each point on the board.
+                #
+                def check_cache(shape_index, jet_index, board):
+                    pattern = []
+
+                    # Loop through the last 20 rows of the board
+                    for y in range(board.top - CACHE_CHECK_RANGE, board.top + 1):
+                        row = []
+
+                        self.row_to_string(y, row)
+
+                        pattern.append(''.join(row))
+
+                    pattern_string = ("\n".join(pattern[::-1]))
+
+                    if (shape_index, jet_index, pattern_string) in self.pattern_cache:
+                        # Found a repeating pattern
+                        self.repeat_found = True
+                        print(f"self.pattern_cache[(shape_index, jet_index, pattern_string)] = {self.pattern_cache[(shape_index, jet_index, pattern_string)]}")
+
+                        return (True, self.pattern_cache[(shape_index, jet_index, pattern_string)])
+                    else:
+                        # Add the pattern to the cache.
+                        # Data is the current board top, and the number of shapes and jets that have been processed
+                        # Store in the cache with the key (shape_index, jet_index, pattern_string):
+                        #   - The current board height
+                        #   - The number of shapes processed
+                        self.pattern_cache[(shape_index, jet_index, pattern_string)] = (self.top, len(self.resting_shapes))
+
+                        # print(self.pattern_cache[(shape_index, jet_index, pattern_string)])
+                        return (False, None)
+
+                if self.find_repeating_pattern:
+
+                    if self.top > CACHE_CHECK_RANGE:
+                        # Check if the cache check returns true
+                        cache_response = check_cache(shape_index, jet_index, self)
+
+                        # cache_response[0] = True if a repeating pattern was found
+                        # cache_response[1] = The data from the cache (top of board, number of shapes processed)
+                        if cache_response[0] == True:
+                            # Found a repeating pattern
+
+                            print(f"Repeat found at shape number {len(self.resting_shapes)} with a board height of {self.top}")
+
+                            # Height of repeating pattern is the current board height minus the board height when the pattern was first seen
+                            height_of_repeating_pattern = self.top - cache_response[1][0]
+                            print(f"Height of repeating pattern = {height_of_repeating_pattern} (Repeat @ {len(self.resting_shapes)} - Cache hit @ {cache_response[1][0]})")
+
+                            # Number of shapes in repeating pattern is the number of shapes processed minus the number of shapes processed when the pattern was first seen
+                            num_shapes_in_repeating_pattern = len(self.resting_shapes) - cache_response[1][1]
+                            print(f"Number of shapes in repeating pattern = {num_shapes_in_repeating_pattern}")
+
+                            # Remaining drops is 1 trillion minus the number of shapes currently processed 
+                            num_remaining_drops = 1000000000000 - len(self.resting_shapes)
+                            print(f"Number of remaining drops = {num_remaining_drops}")
+                    
+                            # Repeats required is the number of remaining drops divided by the number of shapes in the repeating pattern
+                            num_repeats_required = num_remaining_drops // num_shapes_in_repeating_pattern
+                            print(f"Number of repeats required = {num_repeats_required}")
+
+                            # Height of repeated pattern is the number of repeats required multiplied by the height of the repeating pattern
+                            height_of_repeated_pattern = num_repeats_required * height_of_repeating_pattern
+                            print(f"Height of repeated pattern = {height_of_repeated_pattern}")
+
+                            # The remaining drops we need to manually process is the number of remaining drops modulo the number of shapes in the repeating pattern
+                            num_remaining_drops %= num_shapes_in_repeating_pattern
+                            print(f"Number of remaining drops to process = {num_remaining_drops}")
+
+                            # Adjust the height to include the repeated pattern
+                            self.height_offset_required = height_of_repeated_pattern
+                            print(f"Height offset required = {self.height_offset_required}")
+
+                            # Set the value of the number of remaining drops we need to manually process
+                            self.num_remaining_drops = num_remaining_drops
+                            
 
                 # Quit moving this shape
                 break
 
             # Draw the board
             self.draw_board()
+
 
     def draw_board(self):
         if DRAW_BOARD:
@@ -229,6 +319,7 @@ class Board:
             print(str(self))
 
             time.sleep(0.25)     
+
 
     def __str__(self) -> str:
         # Prints out the current board state
@@ -246,23 +337,21 @@ class Board:
             row.append(f"{y:3d}")
 
             if y == Board.FLOOR:
-                # Floor row
+                    # Floor row
                 row.append(Board.PrintableChars.CORNER.value)
                 row.extend([Board.PrintableChars.FLOOR.value] * Board.WIDTH)
                 row.append(Board.PrintableChars.CORNER.value)
             else:
-
-                # Loop through the columns
+                    # Loop through the columns
                 for x in range(Board.L_WALL, Board.R_WALL + 1):
-                    
                     if x == Board.L_WALL or x == Board.R_WALL:
-                        # Draw the left and right walls
+                            # Draw the left and right walls
                         row.append(Board.PrintableChars.WALL.value)
                     elif Point(x,y) in self.active_shape.points:
-                        # Draw the active shape
+                            # Draw the active shape
                         row.append(Board.PrintableChars.FALLING.value)
                     elif Point(x,y) in self.resting_points:
-                        # Draw the resting shapes
+                            # Draw the resting shapes
                         row.append(Board.PrintableChars.RESTING.value)
                     else:
                         row.append(Board.PrintableChars.EMPTY.value)
@@ -272,31 +361,25 @@ class Board:
         # return the rows as a string in reverse order
         return (f"{repr(self)}" + "\n".join("".join(row) for row in reversed(rows)))
 
+
+    def row_to_string(self, y, row):
+        #
+        # Helper function to convert a row to a string.
+        #
+                # Loop through the columns
+            for x in range(Board.L_WALL, Board.R_WALL + 1):
+                if Point(x,y) in self.active_shape.points:
+                        # Draw the active shape
+                    row.append('#')
+                elif Point(x,y) in self.resting_points:
+                        # Draw the resting shapes
+                    row.append('@')
+                else:
+                    row.append('.')
+
+
     def __repr__(self) -> str:
         return (f"{Colours.BOLD.value}Board: current height = {self.top} {Colours.NORMAL.value} \n")
-
-  
-
-
-
-def part1(data):
-    print("Part 1")
-
-    board = Board(data)
-
-    NUM_SHAPES = 17
-
-    # Loop for NUM_SHAPES
-    for x in range(NUM_SHAPES):
-        board.new_shape()
-
-    return 
-
-
-def part2(data):
-    print("Part 2")
-
-    return
 
 
 def main():
@@ -308,8 +391,48 @@ def main():
     with open(input_file) as f:
         data = f.read()
 
-        part1(data)
-        part2(data)
+    if data:
+        print("Part 1")
+        # Part 1 will create a new board and add shapes to it until the
+        # number of shapes dropped == 2022. The board will then print out
+        # the current height of the board
 
+        board = Board(data)
+
+        NUM_SHAPES = 2022
+
+        # Loop for NUM_SHAPES
+        for x in range(NUM_SHAPES):
+            board.new_shape()
+        
+        print(f"Part 1 - Top of board: {board.top}")
+
+
+        print("Part 2")
+        # Part 2 will start again, but looking to find a repeating pattern
+        # to determine how tall the board will be after 1,000,000,000 shapes 
+        # have been dropped
+
+        board = Board(data)
+        board.find_repeating_pattern = True
+
+        while board.repeat_found == False:
+            board.new_shape()
+
+        board.find_repeating_pattern = False
+
+        NUM_SHAPES = board.num_remaining_drops
+
+        board.debug = False
+
+        for x in range(NUM_SHAPES):
+            board.new_shape()
+
+        print(f"Part 2 - Top of board: {board.top + board.height_offset_required}")
+
+
+
+
+    
 if __name__ == "__main__":
     main()
